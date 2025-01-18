c7ac80184829f391fdd384a4f6b657f3fa98cdc6
Pranav1326 <singhpranav130@gmail.com>
2025-01-18 20:43:23 +0530

    Learn class and default parameter

---
+++ JS/class.js
@@ -0,0 +1,57 @@
+
+
+
+// function sayName(name = "Pranav") {
+//     console.log(name);
+// }
+
+// sayName("Anika");
+
+
+
+function sayName(name = "Pranav" , lname = name.toUpperCase()) {
+    console.log(name , lname);
+}
+
+sayName("Anika");
+
+
+
+
+
+
+
+// class Human{
+
+//     age = 25;
+//     #wt = 80;
+//     ht = 180;
+    
+//     walking() {
+//         console.log("Hello i am walking");
+//     }
+
+//     running() {
+//         console.log("I am running");
+//     }
+
+//     get weight() {
+//         return this.#wt;
+//     }
+
+//     set weight(val) {
+//         this.#wt = val;
+//     }
+// }
+
+// let obj = new Human();
+// console.log(obj.age);
+// obj.walking();
+// // console.log(obj.#wt);
+
+// obj.weight = 50;
+// console.log(obj.weight);
+
+
+
+
