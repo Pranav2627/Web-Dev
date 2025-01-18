f1936b01324f795174af6ba3a3d37fb3850ee6e9
Pranav1326 <singhpranav130@gmail.com>
2025-01-18 20:09:10 +0530

    Learn Scoping and Temporal dead zone

---
+++ JS/variable_scoping_and_temporal_dead_zone.js
@@ -0,0 +1,21 @@
+
+// Global Scope
+// var a = 15; 
+
+// console.log(a);
+
+// {
+//     console.log(a);
+// }
+
+// Function Scope
+
+// function add(a , b) {
+//     let ans = a + b;
+//     console.log(ans);
+// }
+
+// add(2,3);
+
+// console.log(ans);
+
